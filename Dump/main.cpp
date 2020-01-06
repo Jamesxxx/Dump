51f02bc8936b4f728680e213dbd549070c9c87d7
coder1351671 <1312841677@qq.com>
2020-01-06 16:10:35 +0800

    all

---
+++ Dump/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Dump.h"
+
+
+VOID XX()
+{
+	int* p = new int[100];
+}
+
+int main()
+{
+	Dump dump;
+	getchar();
+	
+	//*p = 100;
+	XX();
+	getchar();
+	
+
+	return 0;
+}
